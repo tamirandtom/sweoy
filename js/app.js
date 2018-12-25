f10b12c3c981d70ca4e75fb6a5fe92d043e4181c
tamirp@gmail.com <tamirp@gmail.com>
2018-12-25 10:48:24 +0200

    added illustrations

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-var App = angular.module('SWEOY', ['ngAnimate']);
-App.controller('index', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+var App = angular.module('SWEOY', ['ngAnimate','ngSanitize']);
+App.controller('index', ['$scope', '$http', '$sce', function ($scope, $http, $sce) {
+
 
 $scope.allPages = [
   {
@@ -7,20 +8,20 @@ $scope.allPages = [
     "titlemeta":"communication",
     "title":"Title of this section and another subtitle here",
     "content":"The use of Sketch Symbols has grown by 25% in 2017 and designers started using shared libraries. Over 4 Git-like design version control tools have launched in 2017 to give designers one source of truth. Where did this smart-and-synced design approach come from?",
-    "image":"d"
+    "image":$sce.trustAsHtml(ill2)
   },
   {
     "divid":"p2",
     "titlemeta":"communication",
     "title":"Title of this section and another subtitle here",
     "content":"The use of Sketch Symbols has grown by 25% in 2017 and designers started using shared libraries. Over 4 Git-like design version control tools have launched in 2017 to give designers one source of truth. Where did this smart-and-synced design approach come from?",
-    "image":"d"
+    "image":$sce.trustAsHtml(ill1)
   },  {
     "divid":"p3",
     "titlemeta":"communication",
     "title":"Title of this section and another subtitle here",
     "content":"The use of Sketch Symbols has grown by 25% in 2017 and designers started using shared libraries. Over 4 Git-like design version control tools have launched in 2017 to give designers one source of truth. Where did this smart-and-synced design approach come from?",
-    "image":"d"
+    "image":"<h1>foo</h1>"
   }
 ];
 
@@ -69,5 +70,4 @@ $scope.allPages = [
 
 
 }]);
-
 
